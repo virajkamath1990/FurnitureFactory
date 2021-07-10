b60400cd43fbe577b150c331ab2e8e0aa958e0e6
virajkamath1990 <viraj.kamath58@gmail.com>
2021-07-10 13:31:44 +0530

    Add FurnitureFactory Project

---
+++ src/main/java/hackerrank/FurnitureOrder.java
@@ -0,0 +1,72 @@
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class FurnitureOrder implements FurnitureOrderInterface {
+    /**
+     * TODO: Create a map of Furniture items to order quantities
+     */
+
+	private final HashMap<Furniture,Integer> furnitures;
+	
+    /**
+     * Initialize a new mapping of Furniture types to order quantities.
+     */
+	
+	
+    FurnitureOrder() {
+        // TODO: Complete the constructor
+    	furnitures = new HashMap<Furniture, Integer>();
+    }
+
+    public void addToOrder(final Furniture type, final int furnitureCount) {
+        Integer count = 0;
+        if(furnitures.containsKey(type))
+        {
+        	count = furnitures.get(type);
+        }
+        furnitures.put(type, furnitureCount);
+    }
+
+    public HashMap<Furniture, Integer> getOrderedFurniture() {
+        
+    	return new HashMap<Furniture, Integer>(furnitures);
+    }
+
+    public float getTotalOrderCost() {
+        if(!furnitures.isEmpty())
+        {
+        	return furnitures.entrySet().stream().map(furn->furn.getKey().cost()*furn.getValue()).collect(Collectors.toList()).stream().reduce(Float::sum).get();
+        }
+        return 0.0f;
+    }
+
+    public int getTypeCount(Furniture type) {
+        if(furnitures.containsKey(type))
+        {
+        	return furnitures.get(type);
+        }
+        return 0;
+    }
+
+    public float getTypeCost(Furniture type) {
+        
+    	if(furnitures.containsKey(type))
+    	{
+    		return furnitures.get(type)*type.cost();
+    	}
+    	
+        return 0.0f;
+    }
+
+    public int getTotalOrderQuantity() {
+        
+    	if(!furnitures.isEmpty())
+    	{
+    		return furnitures.values().stream().reduce(Integer::sum).get();
+    	}
+    	
+        return 0;
+    }
+}
---
+++ src/test/java/hackerrank/UnitTestSuite.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   SampleUnitTest.class,
+   PrivateUnitTest.class
+})
+
+public class UnitTestSuite {
+
+	
+	
+}
